9fd107a5bae9babdd2f5f792847c2876cb02a829
0zd0 <67220210+0zd0@users.noreply.github.com>
2024-12-03 16:42:07 +0300

    Fix endpoint

--- src/Constants.php
+++ src/Constants.php
@@ -5,6 +5,6 @@ namespace Onepix\EAvtovokzalApiClient;
 class Constants
 {
     public const PROTOCOL = 'https://';
-    public const BASE_URL_API = 'cluster.avtovokzal.ru/gds114/soap/json?WSDL';
+    public const BASE_URL_API = 'cluster.avtovokzal.ru/gds114/soap/sales?WSDL';
     public const BASE_URL_TEST_API = 'cluster.avtovokzal.ru/gdstest/soap/sales?WSDL';
 }
